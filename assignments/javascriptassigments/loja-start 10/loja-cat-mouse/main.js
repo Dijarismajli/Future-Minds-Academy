dbb13bc787f0e8b7dbda2c9b790cd2556d89a7f6
Dijarismajli <146867124+Dijarismajli@users.noreply.github.com>
2024-06-02 19:23:02 +0200

    loja

--- assignments/javascriptassigments/loja-start 10/loja-cat-mouse/main.js
+++ assignments/javascriptassigments/loja-start 10/loja-cat-mouse/main.js
@@ -7,10 +7,17 @@ let bgReady = false;
 let catReady = false;
 let mouse = false;
 
+gameOver = false;
+let Timer = 15;
+
 bgGame.onload = function () {
     bgReady = true;
 }
 
+let catObj = {};
+catObj.x = 0;
+catObj.y = 40;
+catObj.speed = 5;
 const catImg = new Image();
 catImg.src = "images/cat.png";
 catImg.onload = function () {
@@ -37,10 +44,44 @@ function render() {
         ctx.drawImage(bgGame, 0, 0);
     }
     if (catReady) {
-        ctx.drawImage(catImg, 0, 0);
+        ctx.drawImage(catImg, catObj.x, catObj.y);
     }
     if (mouseReady) {
         ctx.drawImage(mouseImg, mouseObj.x, mouseObj.y);
     }
+
+    if (catObj.x > 500) { catObj.x = -50 }
+    if (catObj.x < -51) { catObj.x = 499 }
+
+    if (catObj.y > 460) { catObj.y = -50 }
+    if (catObj.y < -53) { catObj.y = 407 }
+
+    ctx.font = "20px Gergia";
+    ctx.fillStyle = "White"
+    ctx.fillText("Points:0", 10, 25)
+
+    ctx.font = "20px Gergia";
+    ctx.fillStyle = "White"
+    ctx.fillText("Timer:" + Timer, 400, 25)
 }
-setInterval(render, 10);
+
+
+addEventListener("keydown", function (e) {
+    if (e.key == "ArrowRight") { catObj.x += catObj.speed }
+    if (e.key == "ArrowLeft") { catObj.x -= catObj.speed }
+    if (e.key == "ArrowDown") { catObj.y += catObj.speed }
+    if (e.key == "ArrowUp") { catObj.y -= catObj.speed }
+    if (catObj.x > 500) { catObj.x = -50 }
+    if (catObj.x < -51) { catObj.x = 499 }
+
+    if (catObj.y > 460) { catObj.y = -50 }
+    if (catObj.y < -53) { catObj.y = 407 }
+})
+
+let startTimer = function () {
+    Timer;
+    if (Timer != 0) { Timer--; } else { gameOver = true }
+}
+
+setInterval(render, 1);
+setInterval(startTimer, 1000);
