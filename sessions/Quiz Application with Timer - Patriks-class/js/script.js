9139132323194e8749152ac07c4fc873eb9f2d81
Dijarismajli <146867124+Dijarismajli@users.noreply.github.com>
2024-06-21 19:02:36 +0200

    quiz

--- sessions/Quiz Application with Timer - Patriks-class/js/script.js
+++ sessions/Quiz Application with Timer - Patriks-class/js/script.js
@@ -1,109 +1,92 @@
-const start_btn = document.querySelector(".start_btn");
-const info_box = document.querySelector(".info_box");
-const exit_quiz = document.querySelector(".exit_quiz");
-const continue_btn = document.querySelector("#continue_btn");
-const quiz_box = document.querySelector(".quiz_box");
-const result_box = document.querySelector(".result_box")
-const que_text = document.querySelector(".que_text");
-const option_list = document.querySelector(".option_list");
-const timer_sec = document.querySelector(".timer_sec");
-const time_line = document.querySelector(".time_line");
-const next_btn = document.querySelector(".next_btn");
-const total_que = document.querySelector(".total_que");
-const score_text = document.querySelector(".score_text")
-const restartgame = document.querySelector("#restartgame")
+const start_btn = document.querySelector('.start_btn');
+const info_box = document.querySelector('.info_box');
+const exit_quiz = document.querySelector('.exit_quiz');
+const continue_btn = document.querySelector('#continue_btn');
+const quiz_box = document.querySelector('.quiz_box');
+const timer_sec = document.querySelector('.timer_sec');
+const que_text = document.querySelector('.que_text');
+const option_list = document.querySelector('.option_list');
+const total_que = document.querySelector('.total_que');
+const next_btn = document.querySelector('.next_btn');
+const result_box = document.querySelector('.result_box');
+const score_text = document.querySelector('.score_text');
 
-let timerInterval;
-let points = 0;
-let totalNumOfQuestions = questions.length;
-let qCounter = 0;
 let timer = 15;
-let lineTimer = 0;
+let activeQuestion = 0;
+let points = 0;
 
-start_btn.addEventListener("click", function () {
-    info_box.classList.add("activeInfo");
-    start_btn.style.display = "none";
+start_btn.addEventListener('click', function () {
+    info_box.classList.add('activeInfo');
 });
 
-exit_quiz.addEventListener("click", function () {
-    info_box.classList.remove("activeInfo");
-    start_btn.style.display = "block";
+exit_quiz.addEventListener('click', function () {
+    info_box.classList.remove('activeInfo');
 });
 
-continue_btn.addEventListener("click", function () {
-    info_box.classList.remove("activeInfo");
-    quiz_box.classList.add("activeQuiz");
-    loadQuestion(0);
+continue_btn.addEventListener('click', function () {
+    info_box.classList.remove('activeInfo');
+    quiz_box.classList.add('activeQuiz');
+    startQuiz();
+    loadQuestions(activeQuestion);
 });
 
-function loadQuestion(index) {
-    let allOptions = questions[index].options;
-    que_text.innerHTML = questions[index].question;
-    option_list.innerHTML = "";
+next_btn.addEventListener('click', function () {
+    next_btn.classList.remove('show');
+    if (activeQuestion == 4) {
+        next_btn.textContent = 'Finish';
+    }
+
+    if (activeQuestion < questions.length - 1) {
+        activeQuestion++;
+        loadQuestions(activeQuestion);
+    }
+    else {
+        quiz_box.classList.remove('activeQuiz');
+        result_box.classList.add('activeResult');
+        score_text.textContent = points + '/' + questions.length;
+    }
+});
+
+function startQuiz() {
+    let timeCount = setInterval(() => {
+        timer_sec.textContent = timer;
+        if (timer > 0) { timer-- }
+        else { clearInterval(timeCount); }
+    }, 1000);
+}
+
+function loadQuestions(q) {
+
+    let allOptions = questions[q].options;
+    que_text.textContent = questions[q].question;
+    option_list.innerHTML = '';
 
     for (let i = 0; i < allOptions.length; i++) {
-        option_list.innerHTML += `<div class="option" onclick="optionSelect(this,${index},${i})">
-                                                   <span>
-                                                          ${allOptions[i]}
-                                                                    </span></div>`;
+        option_list.innerHTML += `
+            <div class="option" onclick="checkAnswer(${q}, ${i}, this)">
+                <span>${allOptions[i]}</span>
+            </div>
+        `;
     }
-    timerInterval = setInterval(timerCount, 1000);
+    total_que.textContent = questions[q].numb + '/' + questions.length + ' questions';
 }
 
-let timerCount = function () {
-    if (timer > 0) { timer--; }
-    timer_sec.textContent = timer;
+function checkAnswer(q, o, opt) {
+    console.log(opt);
+    next_btn.classList.add('show');
 
-    if (lineTimer < 550) { lineTimer += 550 / 15; }
-    time_line.style.width = lineTimer + "px";
-}
-
-let optionSelect = function (opt, q, o) {
-    for (let i = 0; i < 4; i++) {
+    for (let i = 0; i < questions[q].options.length; i++) {
+        option_list.children[i].classList.add('disabled');
         if (questions[q].options[i] == questions[q].answer) {
-            option_list.childNodes[i].classList.add("correct")
+            option_list.children[i].classList.add('correct');
         }
-        option_list.childNodes[i].classList.add("disabled");
     }
-
-    if (questions[q].options[o] == questions[q].answer) {
+    if (questions[q].answer == questions[q].options[o]) {
+        opt.classList.add('correct');
         points++;
     }
-    if (questions[q].options[o] != questions[q].answer) {
-        opt.classList.add("incorrect")
+    else {
+        opt.classList.add('incorrect');
     }
 
-
-    clearInterval(timerInterval);
-    qCounter++;
-
-    next_btn.classList.add("show");
-    total_que.innerHTML = qCounter + " / 6 questions";
-    if (qCounter == totalNumOfQuestions) {
-        quiz_box.classList.remove("activeQuiz");
-        result_box.classList.add("activeResult")
-        score_text.textContent = points + " from " + totalNumOfQuestions;
-    }
 }
-
-next_btn.addEventListener("click", function () {
-    loadQuestion(qCounter)
-    resetAllQuestion();
-});
-
-let resetAllQuestion = function () {
-    timer_sec.textContent = 15;
-    timer = 16;
-    lineTimer = 0;
-    next_btn.classList.remove("show");
-    time_line.style.width = 0;
-    clearInterval(timerInterval);
-    timerInterval = setInterval(timerCount, 1000);
-}
-
-restartgame.addEventListener(
-    "click", function () {
-        window.location.reload();
-    }
-
-)
