332c9c9d7d7f63cedc0feca6e4264daaa28c2f51
Dijarismajli <146867124+Dijarismajli@users.noreply.github.com>
2025-04-22 16:37:50 +0200

    react proejct 2

---
+++ Ushtrime React/new-app/my-app/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Card from './card';
+import './card.css'
+function App() {
+
+  return (<Card />)
+
+}
+
+export default App;
---
+++ Ushtrime React/new-app/my-app/src/card.js
@@ -0,0 +1,16 @@
+function Card() {
+    return (
+
+        <>
+            <div className="datinjo">
+                <h2>Dati.com</h2>
+                <p>Dat Vasiliiii</p>
+                <h2>A tu ka rrudh bilbili</h2>
+                <h2>Fitoj gjesti aaaaaaa</h2>
+
+            </div>
+        </>
+    );
+
+}
+export default Card
