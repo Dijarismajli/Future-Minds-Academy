25ba97a09ecb3365b20f134cc1f9bf1732081c69
Dijarismajli <146867124+Dijarismajli@users.noreply.github.com>
2025-05-10 11:51:20 +0200

    ushtrime me profen

---
+++ Ushtrime React/ushtrime-me-profen/src/Components/Counter.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+
+    const increment = () => count < 10 && setCount(count + 1);
+    const decrement = () => count > 0 && setCount(count - 1);
+    return (
+
+        <>
+            <h1>Counting</h1>
+            <hr />
+            <div >
+                <button onClick={decrement}>-</button>
+                <h2>{count}</h2>
+                <button onClick={increment}>+</button>
+            </div>
+        </>
+    )
+}
+
+export default Counter
